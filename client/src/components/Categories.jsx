7b245b4ee9fbc922c7d05f79726da7c0fb800f48
shailendrago100 <shailendragoyal1000@gmail.com>
2023-05-09 14:42:40 +0530

    touch 2

--- client/src/components/Categories.jsx
+++ client/src/components/Categories.jsx
@@ -7,26 +7,26 @@ const Categories = () => {
     <section className='containCat' id='categories'>
       <div className='grid gap-2 md:grid-cols-3 mb-2'>
         <Categorie
-          name='Clothes'
+          name='tshirt'
           image='https://picsum.photos/id/221/640/480'
         />
         <Categorie
-          name='Shoes'
-          image='https://picsum.photos/id/21/640/480'
+          name='shoes'
+          image='https://res.cloudinary.com/diyixgmes/image/upload/v1683619950/NIKE_Men_s_Dunk_High_Pro_SB_305050_373_Pine_Green_Metallic_Gold_Sneaker_nkrzht.jpg'
         />
         <Categorie
-          name='Electronics'
+          name='jeans'
           image='https://picsum.photos/id/271/640/480'
         />
       </div>
       <div className='grid gap-2 md:grid-cols-2'>
         <Categorie
-          name='Furniture'
+          name='hoodies'
           image='https://picsum.photos/id/214/640/480'
         />
         <Categorie
-          name='Others'
-          image='https://picsum.photos/id/291/640/480'
+          name='others'
+          image='https://res.cloudinary.com/diyixgmes/image/upload/v1683619951/Titan_Analog_Black_Dial_Men_s_Watch_hdxd4p.jpg'
         />
       </div>
     </section>
