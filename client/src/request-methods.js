adf16887077f972692a9a016d9ca22960ecdab96
nikhilmittalx <nikhilmittal421@gmail.com>
2023-06-23 11:33:25 +0530

    backend link updated

--- client/src/request-methods.js
+++ client/src/request-methods.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://ecom-back-sd62.onrender.com/api';
+const BASE_URL = 'https://back-master-blond.vercel.app/api';
+// const BASE_URL = 'https://ecom-back-sd62.onrender.com/api';
 const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzMGEzOGUwYjczZTg5NTczZDAxNmZmNSIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY2MzA3NTI3OSwiZXhwIjoxNjYzMTYxNjc5fQ.sLeBTfgAymobkEF1rcOzexB8-ixEXKWat-7lRFfaKVQ';
 
 export const publicRequest = axios.create(
